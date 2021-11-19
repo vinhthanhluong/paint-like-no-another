cbdba9378b32923a78d0038e1a48dd7f23a0172b
thành <vinhthanhluong@gmail.com>
2021-11-19 22:45:50 +0700

    update 19-11 lan 3

--- template/js/modules/CartBagModule.js
+++ template/js/modules/CartBagModule.js
@@ -1,29 +1,29 @@
 export default function CartBagModule() {
     // popup-big
-    // $('.header-cart').on('click', function () {
-    //     $('.cart-popup').css('right', 0);
-    //     $('body').addClass('modal-open');
-    // });
+    $('.heart-title').on('click', function () {
+        $('.heart-popup').css('right', 0);
+        $('body').addClass('modal-open');
+    });
 
-    // $('.cart-popup .btn-close , .bg-cart').on('click', function () {
-    //     $('.cart-popup').css('right', "-100%");
-    //     $('body').removeClass('modal-open');
-    // });
+    $('.heart-popup .btn-close , .bg-cart').on('click', function () {
+        $('.heart-popup').css('right', "-100%");
+        $('body').removeClass('modal-open');
+    });
 
     //popup-small
-    if ($('.header-cart')) {
+    // if ($('.header-heart')) {
 
-        // const $cart = $('.header-cart');
-        // $(document).mouseup(function (e) { 
-        //     // nếu click vàp mục tiêu k phải là vùng chứa và cũng k phải là con của vùng chứa
-        //     if (!$cart.is(e.target) &&  $cart.has(e.target).length === 0) {
-        //         $cart.removeClass('active');
-        //     }
-        // });
+    //     const $cart = $('.header-heart');
+    //     $(document).mouseup(function (e) { 
+    //         // nếu click vàp mục tiêu k phải là vùng chứa và cũng k phải là con của vùng chứa
+    //         if (!$cart.is(e.target) &&  $cart.has(e.target).length === 0) {
+    //             $cart.removeClass('active');
+    //         }
+    //     });
 
-        // $('.cart-btn').on('click', function () {
-        //    $cart.toggleClass('active');
-        // });
-    }
+    //     $('.cart-btn').on('click', function () {
+    //        $cart.toggleClass('active');
+    //     });
+    // }
 
 }
