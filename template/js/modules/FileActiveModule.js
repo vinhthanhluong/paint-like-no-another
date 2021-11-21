ab1094b1404f6ead15fddc72608f20a9d66c35f4
thành <vinhthanhluong@gmail.com>
2021-11-21 13:02:10 +0700

    updatea 21-11

--- template/js/modules/FileActiveModule.js
+++ template/js/modules/FileActiveModule.js
@@ -1,5 +1,5 @@
 export default function FileActiveModule() {
-  
+
 
     $('.tab-link-mobi').on('click', function () {
         $('.tab-link').slideToggle();
@@ -8,15 +8,51 @@ export default function FileActiveModule() {
     if ($('.header-user')) {
 
         const $user = $('.header-user');
-        $(document).mouseup(function (e) { 
+        $(document).mouseup(function (e) {
             // nếu click vàp mục tiêu k phải là vùng chứa và cũng k phải là con của vùng chứa
-            if (!$user.is(e.target) &&  $user.has(e.target).length === 0) {
+            if (!$user.is(e.target) && $user.has(e.target).length === 0) {
                 $user.removeClass('active');
             }
         });
 
         $('.header-user').on('click', function () {
-           $user.toggleClass('active');
+            $user.toggleClass('active');
         });
     }
+
+
+    let $width = $(window).width();
+    if ($width < 1200) {
+        $('.box-lang').appendTo('.lang-mobi');
+    } else {
+        $('.box-lang').appendTo('.header-lang');
+    }
+
+
+
+    // video
+    var videos = document.querySelectorAll(".video");
+    if (videos) {
+        for (let video of videos) {
+            video.onplay = () => {
+                video.parentElement.classList.add("playing");
+            }
+            video.onpause = () => {
+                video.parentElement.classList.remove("playing");
+            }
+        }
+    }
+
+    $('.banners-inner').on('click', function () {
+        $(this).find('.video-title').addClass('active');
+    });
+
+    
+
+
 }
+
+
+
+
+
